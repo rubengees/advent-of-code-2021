052998c3d06a6b2a9ca4f49f9860a64f76026fff
Ruben Gees <rubengees7@googlemail.com>
2021-12-04 23:39:06 +0100

    Fix readInput dropping empty lines

--- utils.ts
+++ utils.ts
@@ -1,9 +1,5 @@
 import fs, { PathOrFileDescriptor } from "fs"
 
 export function readInput(path: PathOrFileDescriptor): string[] {
-  return fs
-    .readFileSync(path, "utf-8")
-    .trim()
-    .split("\n")
-    .filter((it) => !!it)
+  return fs.readFileSync(path, "utf-8").trim().split("\n")
 }
