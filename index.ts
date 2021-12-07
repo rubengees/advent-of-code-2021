1c5fafa285f36736fd3ad05905628c969428e971
Ruben Gees <rubengees7@googlemail.com>
2021-12-07 21:38:16 +0100

    Extend cli to generate new days

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { PathLike } from "fs"
 import { Argument, Command, InvalidArgumentError } from "commander"
+import fs, { PathLike } from "fs-extra"
+import inquirer from "inquirer"
 import { readInput } from "./utils"
 
 type Program = (input: string[]) => number
 type ProgramImport = { default: Program }
+type PromptResult = { shouldOverride: boolean }
 
 function parseDay(day: string): string {
   const parsed = parseInt(day)
@@ -17,9 +19,7 @@ function parseDay(day: string): string {
   return day
 }
 
-new Command()
-  .version("1.0.0", "-v, --version")
-  .name("advent-of-code-2021")
+const runCommand = new Command("run")
   .addArgument(new Argument("<day>", "the day to run (should be between 1 and 25)").argParser(parseDay))
   .addArgument(new Argument("<part>", "the part to run").choices(["1", "2"]))
   .addArgument(new Argument("[file]", "a file path with the input"))
@@ -52,4 +52,74 @@ new Command()
 
     console.log(output)
   })
+
+const newCommand = new Command("new")
+  .addArgument(new Argument("<day>", "the day to run (should be between 1 and 25)").argParser(parseDay))
+  .addArgument(new Argument("<part>", "the part to run").choices(["1", "2"]))
+  .action(async (day: string, part: string) => {
+    const dayFile = `
+      export default function day${day}_part${part}(input: string[]): number {
+        return 0
+      }
+    `
+      .replace(/^ {4,6}/gm, "")
+      .trimStart()
+
+    const testFile = `
+      import { readInput } from "../../utils"
+      import day${day}_part${part} from "./index"
+      
+      describe("day ${day} part ${part}", () => {
+        test("example", () => {
+          const example = [
+            ""
+          ]
+      
+          const result = day${day}_part${part}(example)
+      
+          expect(result).toEqual(-1)
+        })
+      
+        test("input", async () => {
+          const input = await readInput("day${day}/input.txt")
+          const result = day${day}_part${part}(input)
+      
+          expect(result).toEqual(-1)
+        })
+      })
+    `
+      .replace(/^ {4,6}/gm, "")
+      .trimStart()
+
+    const dayPath = `day${day}`
+    const partPath = `${dayPath}/part${part}`
+
+    if (await fs.pathExists(partPath)) {
+      const result = await inquirer.prompt<PromptResult>([
+        {
+          type: "confirm",
+          name: "shouldOverride",
+          message: "This solution does already exist. Do you want to override?",
+        },
+      ])
+
+      if (!result.shouldOverride) {
+        return
+      }
+    }
+
+    await fs.mkdirs(partPath)
+
+    await Promise.all([
+      fs.appendFile(`${dayPath}/input.txt`, ""),
+      fs.writeFile(`${partPath}/index.ts`, dayFile),
+      fs.writeFile(`${partPath}/index.spec.ts`, testFile),
+    ])
+  })
+
+new Command()
+  .version("1.0.0", "-v, --version")
+  .name("advent-of-code-2021")
+  .addCommand(runCommand)
+  .addCommand(newCommand)
   .parse(process.argv)
--- utils.ts
+++ utils.ts
@@ -1,13 +1,7 @@
-import fs, { PathOrFileDescriptor } from "fs"
+import fs, { PathOrFileDescriptor } from "fs-extra"
 
 export async function readInput(path: PathOrFileDescriptor): Promise<string[]> {
-  return new Promise((resolve, reject) =>
-    fs.readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data.trim().split("\n"))
-      }
-    })
-  )
+  const file = await fs.readFile(path)
+
+  return file.toString().trim().split("\n")
 }
